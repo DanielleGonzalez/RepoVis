1f158636c0b80d62f29a9f17f8ea75218330775c
Danielle <dng2551@rit.edu>
2017-05-10 21:33:29 -0400

    I realized I did not preprocess the data correctly with the visualizationPreProcessing script so I rewrote it

---
+++ src/VisualizationWebPageTemplate/scripts.js
@@ -0,0 +1,12 @@
+google.charts.load("current", {packages:["timeline"]});
+google.charts.setOnLoadCallback(drawTimeline);
+
+function prs_opened():
+
+function prs_closed():
+
+function prs_per_Contributor():
+
+function calculateStats():
+
+function parseCSVs():
